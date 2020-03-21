13dd98fe71f31335f64ed3110e464c1bb5b7f117
Tyler Hodges <hodges3902@gmail.com>
2020-03-20 23:07:24 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 //jshint esversion:6
 
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('view engine', "ejs");
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  var today = new Date();
-  
-  var options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long"
-  };
-  
-  var day = today.toLocaleDateString("en-US", options);
+  let today = new Date();
 
-  res.render("list", { kindOfDay: day });
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function(req, res) {
+  item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
